64da18b930a896edb0bf64cc56252e4d997ba47c
Mark Megahey <mark@indee.io>
2016-12-07 15:16:01 +0000

    filter tweak

--- src/components/repo_list.js
+++ src/components/repo_list.js
@@ -4,10 +4,10 @@ import RepoListItem from './repo_list_item'
 
 const RepoList = (props) => {
   let counter = 0
-  const result = probs.repositories.filter((repository) => ) {
-    repository.name === //SearchBar Input
-  }
-  const repositoryItems =  result.map((repository) => { //result mapped to the repositoryItems 
+  const result = props.repositories.filter((repository) =>  {
+    repository.name === term //stuck on loading because input is null need a default 
+  })
+  const repositoryItems =  result.map((repository) => { //result mapped to the repositoryItems
     counter++
     return <RepoListItem key={counter} repository={repository} />
   })
